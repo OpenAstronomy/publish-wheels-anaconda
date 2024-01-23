0ca13f1123d4687fed4b10fece108f7e6a9108cd
Conor MacBride <conor@macbride.me>
2024-01-23 20:54:27 +0000

    Sort versions based on upload time

--- remove_old_wheels.py
+++ remove_old_wheels.py
@@ -1,7 +1,11 @@
 # Remove all but the latest N versions from wheels on Anaconda.org
+from datetime import datetime, timezone, timedelta
 
 import click
 from binstar_client.utils import get_server_api
+
+UPLOAD_TIME_FMT = r"%Y-%m-%d %H:%M:%S.%f%z"  # e.g., 2018-10-19 19:03:58.717000+00:00
+MIN_DATETIME = datetime.min.replace(tzinfo=timezone(timedelta(hours=0)))
 
 
 @click.command()
@@ -19,14 +23,24 @@ def remove(user, package, keep, token, dry):
     pkg = api.package(user, package)
 
     # Find versions for which wheels are available
-    pypi_versions = set()
+    pypi_versions = {}
     for file_info in pkg["files"]:
         if file_info["type"] == "pypi":
-            pypi_versions.add(file_info["version"])
-    pypi_versions = sorted(pypi_versions)
+            version = file_info["version"]
+            upload_time = datetime.strptime(
+                file_info["upload_time"],
+                UPLOAD_TIME_FMT,
+            )
+            # Keep date of version's most recent upload
+            pypi_versions[version] = max(
+                pypi_versions.get(version, MIN_DATETIME),
+                upload_time,
+            )
+    pypi_versions = [(upload_time, version) for version, upload_time in pypi_versions.items()]
+    pypi_versions = sorted(pypi_versions)  # sort by upload time (then version)
 
     # Determine versions to remove
-    versions_to_remove = pypi_versions[:-keep]
+    versions_to_remove = [version[1] for version in pypi_versions[:-keep]]
 
     # Remove the files
     for file_info in pkg["files"]:
